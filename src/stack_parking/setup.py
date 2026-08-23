73453a49ec5570c2abbcb6f9684b34bdcf76eb04
shonmaker <sonsm0928@gmail.com>
2026-08-23 19:23:09 +0900

    feat(stack_parking): add ICP parking package metadata
    Part 1/5 of the four-LiDAR ICP parking pipeline. Changes are limited to src/stack_parking.

--- src/stack_parking/setup.py
+++ src/stack_parking/setup.py
@@ -24,6 +24,7 @@ setup(
     entry_points={
         'console_scripts': [
             'stack_parking_node = stack_parking.node:main',
+            'parking_simulator = stack_parking.simulation:main',
         ],
     },
 )
